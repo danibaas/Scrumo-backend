cfea07794c1238423d4889f159ac020bb2245131
Wouter de Bruijn <git@electrogamez.nl>
2020-07-08 20:00:40 +0200

    added graphql

---
+++ src/entities/User.ts
@@ -0,0 +1,8 @@
+export class User {
+    constructor() {
+
+    }
+
+    id: string
+    name: string
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
 // Server can be started with: "npm run serve" This will launch a dev environment. nodemon required (npm install -g nodemon typescript)
+import * as express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
 
-console.log("We got this working.");
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!",
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = express();
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log("Now browse to http://localhost:4000" + server.graphqlPath)
+);
