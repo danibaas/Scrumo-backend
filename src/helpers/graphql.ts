5f87418222d2aa42a26bf2c96d19c51c4409522a
Wouter de Bruijn <git@electrogamez.nl>
2020-07-09 19:32:19 +0200

    Destructured index.ts

---
+++ src/helpers/graphql.ts
@@ -0,0 +1,25 @@
+// Server can be started with: "npm run serve" This will launch a dev environment. nodemon required (npm install -g nodemon typescript)
+import * as express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
+
+export function connect(): Promise<Express.Application> {
+    return new Promise((resolve) => {
+        const typeDefs = gql`type Query {hello: String}`;
+
+        const resolvers = {
+            Query: {
+                hello: () => "Hello world!",
+            },
+        };
+
+        const server = new ApolloServer({ typeDefs, resolvers });
+
+        const app = express();
+        server.applyMiddleware({ app });
+
+        app.listen({ port: 4000 }, () =>
+            console.log("Now browse to http://localhost:4000" + server.graphqlPath)
+        );
+        resolve(app);
+    });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,7 @@
-// Server can be started with: "npm run serve" This will launch a dev environment. nodemon required (npm install -g nodemon typescript)
-import * as express from "express";
-import { ApolloServer, gql } from "apollo-server-express";
+// The index file, run via "npm run serve";'
 
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
+import * as typeorm from "./helpers/typeorm";
 
-const resolvers = {
-  Query: {
-    hello: () => "Hello world!",
-  },
-};
-
-const server = new ApolloServer({ typeDefs, resolvers });
-
-const app = express();
-server.applyMiddleware({ app });
-
-app.listen({ port: 4000 }, () =>
-  console.log("Now browse to http://localhost:4000" + server.graphqlPath)
-);
+typeorm.connect().then(() => {
+    console.log("Database connection is active.");
+});
